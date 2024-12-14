3ab9ffdf0e2ba9461dda1af53a399344646d2557
Nağme Uğurtan <93089935+nagmeu@users.noreply.github.com>
2024-12-15 01:32:18 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
+// Navbar'ı websitesine ait isim ve logoyu belirtmek için kullandık. Bu assessment için gerekli olmasa da, 
+// ihtiyaç duyulması halinde menü, hakkında, iletişim vb. kısımlar bu alanda bulunabilir ve navigasyon açısından daha efektif hale getirilebilirdi.
+
 import React from "react";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      {/* Bu kısımda da responsive durumu korumak için bootstrap kullandık.*/}
       <div className="container">
+        {/* Navbar içeriğini ortalamak için Bootstrap'in container sınıfı kullanılıyor. */}
         <a className="navbar-brand d-flex align-items-center" href="#">
           <img
             src="https://pngimg.com/d/rick_morty_PNG17.png"
